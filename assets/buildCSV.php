99dd05a71bd48ffdb15b21bf0f9af21427678f5e
Daniel Schultz <danielcschultz@joust.io>
2014-04-06 21:41:10 -0400

    capturing all the headers in an array

--- assets/buildCSV.php
+++ assets/buildCSV.php
@@ -9,15 +9,30 @@
 
 
 $handle = fopen('delimited.csv', 'r');
-
+$headers = '';
+// if we successfully opened the file
 if ($handle) {
+  // while we still have lines to read
   while (($buffer = fgets($handle)) !== false) {
+    // note teh newline character (\n) in "ENDHEADERS"
+    // if we find our "ENDHEADERS" line, we break from this loop
+    if (strcmp($buffer, "ENDHEADERS\n") == 0) {
+      break;
+    }
     echo $buffer;
     echo "</br>";
+    // building a string of all the headers
+    $headers .= $buffer;
   }
+
+  // create an array of our headers
+  $headerArray = str_word_count($headers, 1, "_");
+
+  var_dump($headerArray);
+
+  // close our handle
   fclose($handle);
 }
-
 
 
 
