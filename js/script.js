f57b32caac298e8fade243fd9c28ef6b3b820f34
Patrick O'Brien <obrien317@gmail.com>
2014-04-08 00:20:55 -0400

    Adding buildData, JSON output for targetnames and observation numbers

--- js/script.js
+++ js/script.js
@@ -5,4 +5,27 @@
  * all that cool JS goes here!
  */
 
+var chandraData, observationData;
 
+d3.json('../assets/chandraData.json',function(error,data){
+	if (error) {
+		console.log(error);
+	}
+	chandraData = data;
+	loadObservationData();
+});
+
+var loadObservationData = function(){
+	d3.json('../assets/observationData.json',function(error,data){
+		if (error) {
+			console.log(error);
+		}
+		observationData = data;
+		buildVis();
+	});
+}
+
+var buildVis = function(){
+	console.log(chandraData);
+	console.log(observationData);
+}
