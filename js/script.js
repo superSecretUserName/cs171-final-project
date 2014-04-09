685ecbbd9d809f4e229d0bf79e323e1d4adc93a4
Patrick O'Brien <obrien317@gmail.com>
2014-04-09 15:36:13 -0400

    adding click functions

--- js/script.js
+++ js/script.js
@@ -24,17 +24,17 @@ var worldPath = d3.geo.path().projection(worldProjection);
 var graticule = d3.geo.graticule();
 
 var svg = d3.select('#vis').append('svg')
-						.attr('width', width)
-						.attr('height', height);
+		.attr('width', width)
+		.attr('height', height);
 
 // star group
 var starGroup = svg.append('g')
-										.classed('star-group',true)
+		.classed('star-group',true)
 
 // world group
 var worldGroup = svg.append('g')
-										.classed('world-group', true)
-										.attr('transform', 'translate(0,0)');
+		.classed('world-group', true)
+		.attr('transform', 'translate(0,0)');
 
 worldGroup.append('defs').append('path')
     .datum({type: 'Sphere'})
@@ -45,10 +45,20 @@ worldGroup.append('use')
     .attr('class', 'world-path-stroke')
     .attr('xlink:href', '#world-path');
 
-worldGroup.append("path")
+worldGroup.append('path')
     .datum(graticule)
-    .attr("class", "graticule")
+    .attr('class', 'graticule')
     .attr("d", worldPath);
+
+var mouseDown = function(){
+	console.log('mousedown');
+}
+var mouseMove = function(){
+	console.log('mousemove');
+}
+var mouseUp = function(){
+	console.log('mouseup');
+}
 
 d3.json('../assets/world.json',function(error,data){
 	if (error) {
@@ -58,14 +68,18 @@ d3.json('../assets/world.json',function(error,data){
 	console.log(worldData);
 
 	worldGroup.append('path')
-	    			.datum(topojson.feature(worldData, worldData.objects.land))
-      			.attr('class', 'country')
-      			.attr('d', worldPath);	
+	    .datum(topojson.feature(worldData, worldData.objects.land))
+      .attr('class', 'country')
+      .attr('d', worldPath)
 
 	worldGroup.append('path')
-	    			.datum(topojson.mesh(worldData, worldData.objects.countries, function(a, b) { return a !== b; }))
-      			.attr('class', 'boundary')
-      			.attr('d', worldPath);	
+	    .datum(topojson.mesh(worldData, worldData.objects.countries, function(a, b) { return a !== b; }))
+      .attr('class', 'boundary')
+      .attr('d', worldPath);
+
+  worldGroup.on('mousedown', mouseDown)
+    	.on('mousemove', mouseMove)
+    	.on('mouseup', mouseUp);
 
 });
 
