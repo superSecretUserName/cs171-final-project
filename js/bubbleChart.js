db87fd4f0ccca25340667a6a560c5346199388b2
Katy Wyman <katy6514@gmail.com>
2014-04-30 08:58:27 -0400

    grabbingi recent changes

--- js/bubbleChart.js
+++ js/bubbleChart.js
@@ -146,10 +146,10 @@ function make_nodes(cycle){
           d3.select("#time").text(d['time'])
           d3.select("#type").text(d['type']);
           d3.select("#hbar")
-            .attr("fill", function(d) { 
+            .style("background-color", function(d) { 
               console.log("d is currently: " + d);
-              //return fill_color(d['category']) ;
-              return "blue";
+              return fill_color(d['category']) ;
+              //return "blue";
             });
           d3.select("#category").text(d['category'])
           d3.select("#tooltip").classed("hidden", false);
